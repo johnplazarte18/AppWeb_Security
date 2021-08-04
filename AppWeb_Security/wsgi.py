675fa8b24c434e176939d187e1901446d82e91c3
jhonp <john.plazarte2017@uteq.edu.ec>
2021-08-04 14:36:22 -0500

    Creacion del proyecto

---
+++ AppWeb_Security/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AppWeb_Security project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppWeb_Security.settings')
+
+application = get_wsgi_application()
